44c73df8bb13e9f702c1ae37493407f5ec8db50a
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2019-07-07 21:46:32 +0200

    refactor test s.t. tests read from the result-file

--- collection_to_doc_vectors/collection_to_doc_vectors.py
+++ collection_to_doc_vectors/collection_to_doc_vectors.py
@@ -3,6 +3,7 @@ sys.path += ['thirdparty/anserini/src/main/python']
 from pyserini.collection import pycollection
 from pyserini.pyclass import JCollections
 from pyserini.index import pygenerator
+import json
 from tqdm import tqdm
 
 
@@ -10,15 +11,21 @@ def documents_in_trec_jsoup_collection(directory):
     collection = pycollection.Collection('TrecCollection', directory)
     generator = pygenerator.Generator('JsoupGenerator')
 
-    for (i, fs) in enumerate(collection):
-        for (i, doc) in enumerate(fs):
-            ret = generator.create_document(doc)
+    for (_, fs) in enumerate(collection):
+        for (_, document) in enumerate(fs):
+            ret = generator.create_document(document)
             if ret is not None:
                 yield ret
 
 
-def transform_documents(dir):
-    ret = [__map_parsed_document(i) for i in documents_in_trec_jsoup_collection(dir)]
+def transform_documents(conf):
+    document_transformer = __map_parsed_document
+    documents = [i for i in documents_in_trec_jsoup_collection(conf['collection_directory'])]
+    ret = [document_transformer(i) for i in documents]
+    __write_to_file(
+        target_file=conf['output_file'],
+        transformed_documents=(document_transformer(i) for i in documents)
+    )
 
     return ret
 
@@ -29,6 +36,12 @@ def __map_parsed_document(document):
         'raw': document.get('raw'),
         'contents': document.get('contents')
     }
+
+
+def __write_to_file(target_file, transformed_documents):
+    with open(target_file, 'w+') as target:
+        for transformed_document in transformed_documents:
+            target.write(json.dumps(transformed_document) + '\n')
 
 
 if __name__ == '__main__':
--- collection_to_doc_vectors/test/test_document_transformation_on_trec_robust04_sample.py
+++ collection_to_doc_vectors/test/test_document_transformation_on_trec_robust04_sample.py
@@ -7,9 +7,19 @@ from collection_to_doc_vectors.collection_to_doc_vectors import *
 from approvaltests import verify_as_json
 
 LA_TIMES_SAMPLE = ['LA010189-0001', 'LA010189-0043', 'LA010189-0097', 'LA010189-0152', 'LA010189-0192']
+OUTPUT_FILE = 'test-output-file'
 
 
 class TestDocumentTransformationOnTrecRobust04Sample(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            os.remove(OUTPUT_FILE)
+        except:
+            pass
+
+    def tearDown(self):
+        self.setUp()
 
     def test_no_transformation(self):
         transformed_document_sample = self.transform_documents_and_select_entries_by_id({
@@ -21,6 +31,15 @@ class TestDocumentTransformationOnTrecRobust04Sample(unittest.TestCase):
 
     @staticmethod
     def transform_documents_and_select_entries_by_id(conf):
-        ret = transform_documents(conf['test_directory'])
+        transform_documents({
+            'collection_directory': conf['test_directory'],
+            'output_file': OUTPUT_FILE
+        })
+
+        ret = []
+
+        with open(OUTPUT_FILE, 'r') as file:
+            for line in file:
+                ret += [json.loads(line)]
 
         return [i for i in ret if i['docid'] in conf['ids']]
